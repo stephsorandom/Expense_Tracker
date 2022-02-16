4e08beeabb6bb2db3241812c521abc83fae81ff9
Stephanie Mitchell <stephsorandom@gmail.com>
2022-02-15 23:55:14 -0500

    styling of balance box done. next is adding state

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,18 @@
-import React, {useState} from 'react';
-import '../index.css'
+import React, { useState } from 'react';
+import '../index.css';
 
 const TransactionView = () => {
     return (
         <div className='addView'>
             <input placeholder='Amount' />
             <input placeholder='Description' />
+            <div className='radioBtn'>
+                <input type='radio' id='expense' name='type' value='EXPENSE' />
+                <label htmlfor='expense'>Expense</label>
+
+                <input type='radio' id='income' name='type' value='EXPENSE' />
+                <label htmlfor='income'>Income</label>
+            </div>
         </div>
         
     )
@@ -16,13 +23,12 @@ function Home(props) {
     return (
         <div className='balanceContainer'>
             <div className='balanceBox'><span>Balance: $10000</span>
-           <button className='addTransaction'>{addTransactVisable ? 'CANCEL' : 'ADD'}</button></div>
-           
-           <div><TransactionView /></div>
-           
+                <button className='addTransaction'>{addTransactVisable ? 'CANCEL' : 'ADD'}</button>
+            </div>
+            <div><TransactionView /></div>
         </div>
     );
-    
+
 }
 
 export default Home;
